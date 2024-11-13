91d2727660b21af4aac7913671fd02ea99b63e5c
Adharika11 <adhs.gs2003@gmail.com>
2024-11-13 10:33:17 +0530

    Consumer created

--- src/main/java/org/example/springbootkafka/config/KafkaTopicConfig.java
+++ src/main/java/org/example/springbootkafka/config/KafkaTopicConfig.java
@@ -1,11 +1,14 @@
 package org.example.springbootkafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 
 public class KafkaTopicConfig {
+    @Bean
     public NewTopic javaguidesTopic(){
         return TopicBuilder.name("javaguides")
+                //.partition(5) - but here we are making default partitions given by kafka
                 .build();
     }
 }
